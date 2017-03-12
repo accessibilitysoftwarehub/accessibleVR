1dd62193549c2b790af2370119f11065f0af8d98
Aidan Smith (11004917) <SMITHAE4@student.op.ac.nz>
2017-03-13 10:32:32 +1300

    editing reticle

--- Assets/Reticle.cs
+++ Assets/Reticle.cs
@@ -5,16 +5,29 @@ using UnityEngine;
 public class Reticle : MonoBehaviour {
 
     public Camera cameraFacing;
-
+    private Vector3 originalScale;
 	// Use this for initialization
 	void Start () {
-		
+        originalScale = transform.localScale;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        RaycastHit hit;
+        float distance;
+        if (Physics.Raycast (new Ray(cameraFacing.transform.position, cameraFacing.transform.rotation * Vector3.forward), out hit))
+        {
+            distance = hit.distance;
+        }
+        else
+        {
+            distance = cameraFacing.farClipPlane * 0.95f;
+        }
+
         transform.LookAt(cameraFacing.transform.position);
-        transform.position = cameraFacing.transform.position + cameraFacing.transform.rotation * Vector3.forward * 2;
+        transform.position = cameraFacing.transform.position + cameraFacing.transform.rotation * Vector3.forward * distance;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+        transform.localScale = originalScale * distance;
     }
 }
