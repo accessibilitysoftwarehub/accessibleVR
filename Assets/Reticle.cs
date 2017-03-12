d35a16d15fe2337534afcc4cb54b28ce5d10ff15
Aidan Smith (11004917) <SMITHAE4@student.op.ac.nz>
2017-03-13 09:59:44 +1300

    Added code for reticle

---
+++ Assets/Reticle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour {
+
+    public Camera cameraFacing;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(cameraFacing.transform.position);
+        transform.position = cameraFacing.transform.position + cameraFacing.transform.rotation * Vector3.forward * 2;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+}
